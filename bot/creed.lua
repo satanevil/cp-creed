2237009a41a69690770c7c37639a052dbb137343
satanevil <satanevil150@gmail.com>
2016-02-16 18:22:59 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,7 +236,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = {144132967,139763200,107264463},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
